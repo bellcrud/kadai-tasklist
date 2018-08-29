d420b40f1076e2caea4e00703d07f576a279620f
shoichi.okura <sofootballso@yahoo.co.jp>
2018-08-29 10:57:43 +0000

    bootstrap

--- config/app.php
+++ config/app.php
@@ -225,7 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        timezone => 'Asia/Tokyo',
+        'timezone' => 'Asia/Tokyo',
 
     ],
 
